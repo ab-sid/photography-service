acaa10d94b0c705edcfe04b6e8a998a38bc40116
ab-sid <absid24@gmail.com>
2022-11-10 12:19:00 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAOV5H4TUG1Tcwpb3HNd0-yL2-CvrgcDU4",
-    authDomain: "assignment-11-c966f.firebaseapp.com",
-    projectId: "assignment-11-c966f",
-    storageBucket: "assignment-11-c966f.appspot.com",
-    messagingSenderId: "41600533755",
-    appId: "1:41600533755:web:1f610931117bf862a2dc5e"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
